8ffff8f13b95fe955b89e8cb2a2c5e0a78d6ab05
ChuckTheBoss <collin.mathews@gmail.com>
2023-05-25 15:47:05 -0600

    got server.js to run. Adds several starter code taken from Leons todo list project under MIT license

--- src/server.js
+++ src/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
+const connectDB = require('../model/connectDB');
 const React = require('react');
 const ReactDOM = require('react-dom');
-const PORT = 3000
+const homeRoutes = require('../route/home')
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 app.listen(PORT, () => {
-    console.log("server listening on port 3000")
+    console.log(`Server listening on port ${PORT}`)
 })
