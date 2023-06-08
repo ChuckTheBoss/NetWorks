911941d5dba0ecb183dcf319b2fde116a9104ee1
ChuckTheBoss <collin.mathews@gmail.com>
2023-06-08 14:53:31 -0600

    Fix server listening

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const connectDB = require('../model/connectDB');
+const connectDB = require('./model/connectDB');
 const React = require('react');
 const ReactDOM = require('react-dom');
-const homeRoutes = require('../route/home')
+const homeRoutes = require('./route/home')
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'))
