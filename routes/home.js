d052a0e11122a7593b3c23247e0703090c12ba98
ChuckTheBoss <collin.mathews@gmail.com>
2023-06-08 15:44:57 -0600

    Server connects and the landing page renders

--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const homeController = require('../controller/home')
+const homeController = require('../controllers/home')
 
 router.get('/', homeController.getIndex)
 
--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
-const connectDB = require('../model/connectDB');
+const connectDB = require('./models/connectDB');
 const React = require('react');
 const ReactDOM = require('react-dom');
-const homeRoutes = require('../route/home')
+const homeRoutes = require('./routes/home')
 const PORT = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
+app.use("/", homeRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
