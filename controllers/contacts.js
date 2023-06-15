42105feb7b6682ff63efeda711be812442722896
ChuckTheBoss <collin.mathews@gmail.com>
2023-06-15 16:35:49 -0600

    Adds a contact page which shows a list of contacts. So far it's just the raw JSON but I'll fix that later

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,6 +1,15 @@
 const Contact = require('../models/contact');
 
 module.exports = {
+    getContacts: async (req, res) => {
+        //console.log(Contact.find())
+        try {
+            const contacts = await Contact.find()
+            res.render('contacts.ejs', { contacts: contacts })
+        } catch (err) {
+            console.log(err)
+        }
+    },
     createContact: async (req, res) => {
         try {
             await Contact.create({
@@ -9,7 +18,7 @@ module.exports = {
                 email: req.body.email,
             })
             console.log('Contact has been added!')
-            //res.redirect('/contact')
+            res.redirect('/contacts')
         } catch (err) {
             console.error(err)
         }
--- routes/contacts.js
+++ routes/contacts.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const contactsController = require('../controllers/contacts')
 
+router.get('/', contactsController.getContacts)
 router.post('/create', contactsController.createContact)
 
 module.exports = router
